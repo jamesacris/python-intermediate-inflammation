7da1727f61120f43fec744e5b0a3626410819254
James Acris <james.acris@stfc.ac.uk>
2022-11-01 19:12:08 +0000

    changes to unit tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -48,3 +48,17 @@ def test_daily_mean_string():
 
     with pytest.raises(TypeError):
         error_expected = daily_min([['hello', 'there'], ['general', 'kenobi']])
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]),
+        ([[1, 1, 1], [1, 1, 1], [1, 1, 1]], [[1, 1, 1], [1, 1, 1], [1, 1, 1]]),
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]])
+    ]
+)
+def test_patient_normalise(test, expected):
+    """Test normalisation works for arrays of positive integers"""
+    from inflammation.models import patient_normalise
+    npt.assert_almost_equal(patient_normalise(test), np.array(expected), decimal=2)
